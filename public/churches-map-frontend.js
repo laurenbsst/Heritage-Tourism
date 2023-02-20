bfd9c849805db4daa8c3a4227c7dcf4dd237b5d9
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-02-20 15:54:22 +0000

    Added map and markers to churches tour page

--- public/churches-map-frontend.js
+++ public/churches-map-frontend.js
@@ -7,6 +7,17 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById("map"), options);
 
+  function pinSymbol(color) {
+    return {
+        path: 'M 0,0 C -2,-20 -10,-22 -10,-30 A 10,10 0 1,1 10,-30 C 10,-22 2,-20 0,0 z M -2,-30 a 2,2 0 1,1 4,0 2,2 0 1,1 -4,0',
+        fillColor: color,
+        fillOpacity: 1,
+        strokeColor: '#000',
+        strokeWeight: 2,
+        scale: 1
+   };
+}
+
   var locationMarker = null;
 
   function autoUpdate() {
@@ -25,11 +36,9 @@ function initMap() {
           locationMarker = new google.maps.Marker({
             position: newPoint,
             map: map,
+            icon: pinSymbol("#C3D6A0")
           });
         }
-
-        // Center the map on the new position
-        map.setCenter(newPoint);
       },
       errorHandler,
       options
@@ -45,93 +54,59 @@ function initMap() {
     position: { lat: 52.6259, lng: 1.2956 },
     map: map,
     url: "/gallery/1",
-  });
-
-  const stPeterMancroftChurch = new google.maps.Marker({
-    position: { lat: 52.6279, lng: 1.2925 },
-    map: map,
-    url: "/gallery/1",
+    label: "1"
   });
 
   const stStephensChurch = new google.maps.Marker({
     position: { lat: 52.6266, lng: 1.2922 },
     map: map,
     url: "/gallery/1",
+    label: "2"
+  });
+
+  const stPeterMancroftChurch = new google.maps.Marker({
+    position: { lat: 52.6279, lng: 1.2925 },
+    map: map,
+    url: "/gallery/1",
+    label: "3"
   });
 
   const stAndrewsChurch = new google.maps.Marker({
     position: { lat: 52.6305, lng: 1.2955 },
     map: map,
     url: "/gallery/1",
-  });
-
-  const stBenedictsChurch = new google.maps.Marker({
-    position: { lat: 52.6312, lng: 1.2861 },
-    map: map,
-    url: "/gallery/1",
+    label: "4"
   });
 
   const stLaurencesChurch = new google.maps.Marker({
     position: { lat: 52.6311, lng: 1.2901 },
     map: map,
     url: "/gallery/1",
+    label: "5"
   });
 
   const stMargaretsChurch = new google.maps.Marker({
     position: { lat: 52.6315, lng: 1.2887 },
     map: map,
     url: "/gallery/1",
+    label: "6"
+  });
+
+  const stBenedictsChurch = new google.maps.Marker({
+    position: { lat: 52.6312, lng: 1.2861 },
+    map: map,
+    url: "/gallery/1",
+    label: "7"
   });
 
   const stGregorysChurch = new google.maps.Marker({
     position: { lat: 52.6303, lng: 1.2888 },
     map: map,
     url: "/gallery/1",
+    label: "8"
   });
 
-  const theCastle = new google.maps.Marker({
-    position: { lat: 52.6287, lng: 1.2962 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const theGuildhall = new google.maps.Marker({
-    position: { lat: 52.6291, lng: 1.2924 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const angliaHouse = new google.maps.Marker({
-    position: { lat: 52.6289, lng: 1.2984 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const cityHall = new google.maps.Marker({
-    position: { lat: 52.6293, lng: 1.2979 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const theForum = new google.maps.Marker({
-    position: { lat: 52.6278, lng: 1.2911 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const strangersHall = new google.maps.Marker({
-    position: { lat: 52.6307, lng: 1.2925 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  const theGreatHospital = new google.maps.Marker({
-    position: { lat: 52.6327, lng: 1.3043 },
-    map: map,
-    url: "/gallery/1",
-  });
-
-  google.maps.event.addListener(theGreatHospital, "click", function () {
+  google.maps.event.addListener(allSaintsChurch, "click", function () {
     window.location.href = this.url;
   });
 }
