713cf19613b203590d34b60879458cdb7b900f7f
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-05-04 17:16:08 +0100

    Added content to modal

---
+++ public/gallery-modal.js
@@ -0,0 +1,93 @@
+function showPreview(clicked_id){
+    if(clicked_id == '1'){
+        document.getElementById('modal-title').innerHTML = "All Saints' Church";
+        document.getElementById('modal-info-text').innerHTML = "All Saint’s Church is a Grade I listed redundant parish church in the Church of England situated in the heart of Norwich city centre.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/all-saints.jpg";
+        document.getElementById('learn-more-link').href="/gallery/1";
+    }
+    if(clicked_id == '2'){
+        document.getElementById('modal-title').innerHTML = "St Peter Mancroft Church";
+        document.getElementById('modal-info-text').innerHTML = "St Peter Mancroft is the largest of the city's 31 surviving medieval parish churches and one of the finest perpendicular parish churches in Norfolk.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/peter-mancroft-church.jpg";
+        document.getElementById('learn-more-link').href="/gallery/2";
+    }
+    if(clicked_id == '3'){
+        document.getElementById('modal-title').innerHTML = "St Stephen's Church";
+        document.getElementById('modal-info-text').innerHTML = "The St Stephen’s Church is a grade I listed building that has stood for 900 years, with its first mention documented in a 12th century royal charter.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-stephens.jpg";
+        document.getElementById('learn-more-link').href="/gallery/3";
+    }
+    if(clicked_id == '4'){
+        document.getElementById('modal-title').innerHTML = "St Andrew's Church";
+        document.getElementById('modal-info-text').innerHTML = "St Andrew’s Church is a 15th/16th century grade I listed building and the second largest medieval parish church in Norwich.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-andrews.jpg";
+        document.getElementById('learn-more-link').href="/gallery/4";
+    }
+    if(clicked_id == '5'){
+        document.getElementById('modal-title').innerHTML = "St Benedict's Church";
+        document.getElementById('modal-info-text').innerHTML = "St Benedict's Church, Norwich is a Grade I listed Anglican former parish church in Norwich, Norfolk, England.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-benedicts.jpg";
+        document.getElementById('learn-more-link').href="/gallery/5";
+    }
+    if(clicked_id == '6'){
+        document.getElementById('modal-title').innerHTML = "St Laurence's Church";
+        document.getElementById('modal-info-text').innerHTML = "A vast, handsome medieval city church and prominent Norwich landmark, St Laurence's has a mighty tower, a clerestory of 11 windows on each side, a long continuous roof covering nave and chancel and two-storied porches to north and south, all set on a steeply sloping site.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-laurences.jpg";
+        document.getElementById('learn-more-link').href="/gallery/6";
+    }
+    if(clicked_id == '7'){
+        document.getElementById('modal-title').innerHTML = "St Margaret's Church";
+        document.getElementById('modal-info-text').innerHTML = "St Margaret's Church, Norwich is a medieval Grade I listed redundant parish church in the Church of England in Norwich. It was closed for five or six years and reopened in 1868 after reconstruction works.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-margarets.jpg";
+        document.getElementById('learn-more-link').href="/gallery/7";
+    }
+    if(clicked_id == '8'){
+        document.getElementById('modal-title').innerHTML = "St Gregory's Church";
+        document.getElementById('modal-info-text').innerHTML = "St Gregory's Church, Norwich is a Grade I listed redundant parish church in the Church of England in Norwich. The church, which stands between Pottergate and St Benedict's Street, is medieval.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/st-gregorys.jpg";
+        document.getElementById('learn-more-link').href="/gallery/8";
+    }
+
+    if(clicked_id == '9'){
+        document.getElementById('modal-title').innerHTML = "Norwich Castle";
+        document.getElementById('modal-info-text').innerHTML = "Norwich Castle is a medieval royal fortification in the city of Norwich, in the English county of Norfolk. William the Conqueror (1066–1087) ordered its construction in the aftermath of the Norman Conquest of England.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/castle.jpg";
+        document.getElementById('learn-more-link').href="/gallery/9";
+    }
+    if(clicked_id == '10'){
+        document.getElementById('modal-title').innerHTML = "Norwich Guildhall";
+        document.getElementById('modal-info-text').innerHTML = "Norwich Guildhall is a Grade I listed municipal building on Gaol Hill. The guildhall was commissioned after King Henry IV awarded a charter to the City of Norwich giving it autonomy from the county of Norfolk.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/guildhall.jpg";
+        document.getElementById('learn-more-link').href="/gallery/10";
+    }
+    if(clicked_id == '11'){
+        document.getElementById('modal-title').innerHTML = "Anglia House";
+        document.getElementById('modal-info-text').innerHTML = "Anglia House was built as an Agricultural Hall in 1882 by J. B. Pearce, of red brick with Cumberland stone facings.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/anglia-television.jpg";
+        document.getElementById('learn-more-link').href="/gallery/11";
+    }
+    if(clicked_id == '12'){
+        document.getElementById('modal-title').innerHTML = "City Hall";
+        document.getElementById('modal-info-text').innerHTML = "Norwich City Hall is an Art Deco building completed in 1938 which houses the city hall for the city of Norwich, East Anglia, in Eastern England. It is one of the Norwich 12, a collection of twelve heritage buildings in Norwich deemed of particular historical and cultural importance.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/norwich-city-hall.jpg";
+        document.getElementById('learn-more-link').href="/gallery/12";
+    }
+    if(clicked_id == '13'){
+        document.getElementById('modal-title').innerHTML = "The Forum";
+        document.getElementById('modal-info-text').innerHTML = "The Forum is a community building in Norwich, Norfolk in England. It stands opposite St Peter Mancroft Church. Built on the site of the previous Norwich Library which burnt down in 1994, the Forum was designed by Michael Hopkins and Partners, and built as a millennium project for the East of England, being finished in October 2001.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/norwich-the-forum.jpg";
+        document.getElementById('learn-more-link').href="/gallery/13";
+    }
+    if(clicked_id == '14'){
+        document.getElementById('modal-title').innerHTML = "Strangers' Hall";
+        document.getElementById('modal-info-text').innerHTML = "One of Norwich's most historic buildings, this magnificent Tudor house has been home to many of the city's leading citizens since the 14th century. There has been a substantial building on this plot since the 13th century or possibly even earlier.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/norwich-strangers-hall.jpg";
+        document.getElementById('learn-more-link').href="/gallery/14";
+    }
+    if(clicked_id == '15'){
+        document.getElementById('modal-title').innerHTML = "The Great Hospital";
+        document.getElementById('modal-info-text').innerHTML = "The Great Hospital was founded in 1249 by Bishop Walter de Suffield with the original beneficiaries being aged priests, poor scholars, and sick and hungry paupers.";
+        document.getElementById('modal-image').src="https://storage.googleapis.com/heritage-tourism-store/norwich-great-hospital.jpg";
+        document.getElementById('learn-more-link').href="/gallery/15";
+    }
+}
