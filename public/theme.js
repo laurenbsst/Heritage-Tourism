39a44eb45c6cef067fddf1cb6b4c343a8ac6138d
Lauren Bassett <58308897+laurenbsst@users.noreply.github.com>
2023-05-21 19:12:01 +0100

    Dark mode functionality

---
+++ public/theme.js
@@ -0,0 +1,14 @@
+import { dark_button } from "./toggle.js";
+let nav = document.getElementById('main-nav');
+
+window.onload = function(e) {
+    if (localStorage.getItem('mode') === 'light') {
+        document.documentElement.setAttribute('data-bs-theme','light');
+        nav.style.backgroundColor = "#C3D6A0";
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark');
+        nav.style.backgroundColor = "#6F7378";
+        dark_button.classList.toggle('active');
+    }
+}
--- public/toggle.js
+++ public/toggle.js
@@ -1,7 +1,25 @@
-const dark_button = document.getElementById('dark_toggle');
+let dark_button = document.getElementById('dark_toggle');
+let nav = document.getElementById('main-nav');
 
-dark_button.addEventListener("click", toggleMode);
+if (dark_button) {
+    dark_button.addEventListener("click", toggleMode);
+}
 
 function toggleMode() {
     dark_button.classList.toggle("active");
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+        nav.style.backgroundColor = "#C3D6A0";
+        localStorage.setItem("nav", '#C3D6A0');
+        localStorage.setItem("mode", 'light');
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+        nav.style.backgroundColor = "#6F7378";
+        localStorage.setItem("nav", '#6F7378');
+        localStorage.setItem("mode", 'dark');
+
+    }
 };
+
+export {dark_button};
